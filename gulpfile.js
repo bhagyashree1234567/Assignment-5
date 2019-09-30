2dc463eaa816ead74b027d85d4b5b2a3f90dc65c
Bhagyashree Mahajan <bhagyashreemahajan123@gmail.com>
2019-09-29 20:23:01 -0700

    added scss files and made provisions for media queries

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+var {src, dest, watch} = require('gulp');
+var gulp = require('gulp')
+var sass = require('gulp-sass');
+sass.complier = require('node-sass');
 
-exports.default = defaultTask
+let srcPath = './src/*.scss'
+let destPath =  './dist/css/'
+
+function css(){
+  return src('./src/*.scss')
+    .pipe(sass({
+      errLogToConsole : true
+    }))
+    .pipe(dest('./dist/css/'))
+};
+
+gulp.task('default',css);
+
+var watcher = gulp.watch(srcPath, ['default']);
+
+// gulp.task('default',['sass'])
+
+// exports.default = css;
